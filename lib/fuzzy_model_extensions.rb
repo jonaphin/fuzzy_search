57a980632a5851dd859a44c80b678daaf5f073e9
David Mike Simon <david.mike.simon@gmail.com>
2011-10-22 11:27:33 -0400

    Moved most of the inner logic into FuzzySearchTrigram

--- lib/fuzzy_model_extensions.rb
+++ lib/fuzzy_model_extensions.rb
@@ -19,7 +19,7 @@ module FuzzySearch
       def fuzzy_searchable_on(*properties)
         # TODO: Complain if fuzzy_searchable_on is called more than once
         named_scope :fuzzy_search_scope, lambda { |words|
-          generate_fuzzy_search_scope_params(words)
+          FuzzySearchTrigram.params_for_search(self, words)
         }
 
         after_save :update_fuzzy_search_trigrams!
@@ -38,15 +38,12 @@ module FuzzySearch
       end
 
       def rebuild_fuzzy_search_index!
-        FuzzySearchTrigram.delete_all(:fuzzy_search_type_id => fuzzy_type_id)
-        all.each do |rec|
-          rec.update_fuzzy_search_trigrams!
-        end
+        FuzzySearchTrigram.rebuild_index(self)
       end
 
       private
 
-      # Retrieve type id, creating it if necessary
+      # Retrieve cached fuzzy type id, creating it if necessary
       def fuzzy_type_id
         r = fuzzy_search_type_id
         unless r
@@ -55,67 +52,15 @@ module FuzzySearch
         end
         r
       end
-
-      def generate_fuzzy_search_scope_params(words)
-        # Quote SQL identifier (i.e. table or column name)
-        def qi(s)
-          connection.quote_column_name(s)
-        end
-
-        # Quote SQL value (i.e. a string or number)
-        def qv(s)
-          connection.quote(s)
-        end
-
-        trigrams = FuzzySearch::split_trigrams(words)
-        # No results for empty search string
-        return {:conditions => "0 = 1"} unless trigrams
-
-        # Transform the list of columns in the searchable entity into 
-        # a SQL fragment like:
-        # "table_name"."id", "table_name"."field1", "table_name"."field2", ...
-        entity_fields = columns.map{|col| "#{qi(table_name)}.#{qi(col.name)}"}.join(",")
-
-        # The SQL expression for calculating fuzzy_score.
-        # Has to be used multiple times because some databases (i.e. Postgres)
-        # do not support HAVING on named SELECT fields.
-        fuzzy_score_expr = "((count(*)*100.0)/#{trigrams.size})"
-
-        # TODO: Optimize this query.
-        # Perhaps better to search primarily on the trigram table alone, then just
-        # return a simple scope that lists ids.
-        return {
-          :select => "#{fuzzy_score_expr} AS fuzzy_score, #{entity_fields}",
-          :joins => "INNER JOIN fuzzy_search_trigrams ON fuzzy_search_trigrams.rec_id = #{qi(table_name)}.#{qi(primary_key)}",
-          :conditions => ["fuzzy_search_trigrams.token IN (?) AND fuzzy_search_type_id = ?",
-            trigrams, fuzzy_type_id],
-          :group => "#{qi(table_name)}.#{qi(primary_key)}",
-          :order => "fuzzy_score DESC",
-          :having => "#{fuzzy_score_expr} >= #{qv(fuzzy_search_threshold)}"
-        }
-      end
     end
 
     module InstanceMethods
       def update_fuzzy_search_trigrams!
-        delete_fuzzy_search_trigrams!
-
-        props = self.class.fuzzy_search_properties.map{|p| send(p)}
-        props = props.select{|p| p and p.respond_to?(:to_s)}
-        trigrams = FuzzySearch::split_trigrams(props)
-
-        FuzzySearchTrigram.import(
-          [:token, :rec_id, :fuzzy_search_type_id],
-          trigrams.map{|t| [t, self.id, self.class.send(:fuzzy_type_id)]},
-          :validate => false
-        )
+        FuzzySearchTrigram.update_trigrams(self)
       end
 
       def delete_fuzzy_search_trigrams!
-        FuzzySearchTrigram.delete_all(
-          :rec_id => self.id,
-          :fuzzy_search_type_id => self.class.send(:fuzzy_type_id)
-        )
+        FuzzySearchTrigram.delete_trigrams(self)
       end
     end
   end
--- lib/fuzzy_search_trigram.rb
+++ lib/fuzzy_search_trigram.rb
@@ -1,3 +1,92 @@
 class FuzzySearchTrigram < ActiveRecord::Base
   belongs_to :fuzzy_search_type
+
+  def self.rebuild_index(type)
+    delete_all(:fuzzy_search_type_id => type.send(:fuzzy_type_id))
+    type.find_each do |rec|
+      update_trigrams(rec)
+    end
+  end
+
+  def self.params_for_search(type, search_term)
+    trigrams = FuzzySearch::split_trigrams(search_term)
+    # No results for empty search string
+    return {:conditions => "0 = 1"} unless trigrams
+
+    q = Quoter.new(type)
+
+    # Transform the list of columns in the searched type into
+    # a SQL fragment like:
+    # "table_name"."id", "table_name"."field1", "table_name"."field2", ...
+    entity_fields = type.columns.map{|col|
+      q.i(type.table_name) + "." + q.i(col.name)
+    }.join(",")
+
+    # The SQL expression for calculating fuzzy_score.
+    # Has to be used multiple times because some databases (i.e. Postgres)
+    # do not support HAVING on named SELECT fields.
+    # TODO: Restore old average thing from the original code; the point
+    # of that was to prefer short matches when given short query strings.
+    # (i.e. "ama" should rank "Amad" higher than "Amalamadingdongwitcherydoo")
+    # The average was between:
+    # the number of trigrams in the query that matched the record, and
+    # the number of trigrams in the record that matched the query!
+    fuzzy_score_expr = "((count(*)*100.0)/#{trigrams.size})"
+
+    return {
+      :select => "#{fuzzy_score_expr} AS fuzzy_score, #{entity_fields}",
+      :joins => "INNER JOIN fuzzy_search_trigrams ON
+                 fuzzy_search_trigrams.rec_id =
+                 #{q.i(type.table_name)}.#{q.i(type.primary_key)}",
+      :conditions => [
+        "fuzzy_search_trigrams.token IN (?)
+        AND fuzzy_search_type_id = ?",
+        trigrams,
+        type.send(:fuzzy_type_id)],
+      :group => "#{q.i(type.table_name)}.#{q.i(type.primary_key)}",
+      :order => "fuzzy_score DESC",
+      :having => "#{fuzzy_score_expr} >= #{q.v(type.fuzzy_search_threshold)}"
+    }
+  end
+
+  def self.update_trigrams(rec)
+    delete_trigrams(rec)
+
+    props = rec.class.fuzzy_search_properties.map{|p| rec.send(p)}
+    props = props.select{|p| p and p.respond_to?(:to_s)}
+    trigrams = FuzzySearch::split_trigrams(props)
+
+    # Ar-extensions import, much much faster than individual creates
+    type_id = rec.class.send(:fuzzy_type_id)
+    import(
+      [:token, :rec_id, :fuzzy_search_type_id],
+      trigrams.map{|t| [t, rec.id, type_id]},
+      :validate => false
+    )
+  end
+
+  def self.delete_trigrams(rec)
+    delete_all(
+      :rec_id => rec.id,
+      :fuzzy_search_type_id => rec.class.send(:fuzzy_type_id)
+    )
+  end
+
+  private
+
+  class Quoter
+    def initialize(type)
+      @type = type
+    end
+
+    # Quote SQL identifier (i.e. table or column name)
+    def i(s)
+      @type.connection.quote_column_name(s)
+    end
+
+    # Quote SQL value (i.e. a string or number)
+    def v(s)
+      @type.connection.quote(s)
+    end
+  end
 end
