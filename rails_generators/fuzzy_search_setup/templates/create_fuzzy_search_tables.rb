1602fe1d840a0295fc843bf10622bc5b96cd15e6
David Mike Simon <david.mike.simon@gmail.com>
2011-10-23 11:49:28 -0400

    Making fuzzy search table columns all not-null

--- rails_generators/fuzzy_search_setup/templates/create_fuzzy_search_tables.rb
+++ rails_generators/fuzzy_search_setup/templates/create_fuzzy_search_tables.rb
@@ -1,15 +1,15 @@
 class CreateFuzzySearchTables < ActiveRecord::Migration
   def self.up
     create_table :fuzzy_search_trigrams, :id => false do |t|
-      t.column :token, :binary, :limit => 3
-      t.column :fuzzy_search_type_id, :integer
-      t.column :rec_id, :integer
+      t.column :token, :binary, :limit => 3, :null => false
+      t.column :fuzzy_search_type_id, :integer, :null => false
+      t.column :rec_id, :integer, :null => false
     end
 
     add_index :fuzzy_search_trigrams, [:fuzzy_search_type_id, :token]
 
     create_table :fuzzy_search_types do |t|
-      t.column :type_name, :string
+      t.column :type_name, :string, :null => false
     end
 
     add_index :fuzzy_search_types, :type_name, :unique => true
