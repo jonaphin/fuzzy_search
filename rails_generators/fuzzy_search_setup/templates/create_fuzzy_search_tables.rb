13fb0f79aeb7665e9beea99f94d742421685b4ae
David Mike Simon <david.mike.simon@gmail.com>
2011-10-25 11:11:04 -0400

    Argh, apparently binary doesn't mean 'binary' to rails migrations

--- rails_generators/fuzzy_search_setup/templates/create_fuzzy_search_tables.rb
+++ rails_generators/fuzzy_search_setup/templates/create_fuzzy_search_tables.rb
@@ -1,12 +1,18 @@
 class CreateFuzzySearchTables < ActiveRecord::Migration
   def self.up
+    is_mysql = ActiveRecord::Base.connection.adapter_name.downcase == 'mysql'
+
     create_table :fuzzy_search_trigrams, :id => false do |t|
-      t.column :token, :binary, :limit => 3, :null => false
+      if is_mysql
+        t.column :token, "binary(3)", :null => false
+      else
+        t.column :token, :binary, :limit => 3, :null => false
+      end
       t.column :fuzzy_search_type_id, :integer, :null => false
       t.column :rec_id, :integer, :null => false
     end
 
-    if ActiveRecord::Base.connection.adapter_name.downcase == 'mysql'
+    if is_mysql
       ActiveRecord::Base.connection.execute(
         "alter table fuzzy_search_trigrams add primary key (fuzzy_search_type_id,token,rec_id)"
       )
