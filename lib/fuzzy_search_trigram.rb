8f102166b4ab2c2469cc6b8b5426f87df1e16b54
David Mike Simon <david.mike.simon@gmail.com>
2011-10-24 20:28:09 -0400

    Removed fuzzy_score_expr since it's no longer necessary

--- lib/fuzzy_search_trigram.rb
+++ lib/fuzzy_search_trigram.rb
@@ -13,21 +13,7 @@ class FuzzySearchTrigram < ActiveRecord::Base
     # No results for empty search string
     return {:conditions => "0 = 1"} unless trigrams
 
-    # The SQL expression for calculating fuzzy_score.
-    # Has to be used multiple times because some databases (i.e. Postgres)
-    # do not support HAVING on named SELECT fields.
-    # Possible TODO: Restore old average thing from the original code; the point
-    # of that was to prefer short matches when given short query strings.
-    # (i.e. "ama" should rank "Amad" higher than "Amalamadingdongwitcherydoo")
-    # The average was between:
-    # the number of trigrams in the query that matched the record, and
-    # the number of trigrams in the record that matched the query!
-    fuzzy_score_expr = "((count(*)*100.0)/#{trigrams.size})"
-#    fuzzy_score_expr = "(((count(*)*100.0)/#{trigrams.size}) + " +
-#    "((count(*)*100.0)/(SELECT count(*) FROM fuzzy_search_trigrams WHERE " +
-#    "rec_id = #{q.i(type.table_name)}.#{q.i(type.primary_key)} AND " +
-#    "fuzzy_search_type_id = #{q.v(type.send(:fuzzy_type_id))})))/2.0"
-
+    # Retrieve the IDs of the matching items
     search_result = connection.select_rows(
       "SELECT rec_id, count(*) FROM #{i(table_name)} " +
       "WHERE token IN (#{trigrams.map{|t| v(t)}.join(',')}) " +
@@ -38,10 +24,10 @@ class FuzzySearchTrigram < ActiveRecord::Base
     )
     return {:conditions => "0 = 1"} if search_result.empty?
 
+    # Perform a join between the target table and a fake table of matching ids
     static_sql_union = search_result.map{|rec_id, count|
       "SELECT #{v(rec_id)} AS fuzzy_search_rec_id, #{v(count)} AS fuzzy_search_score"
     }.join(" UNION ");
-
     return {
       :joins => "INNER JOIN (#{static_sql_union}) ON " +
                 "fuzzy_search_rec_id = #{i(type.table_name)}.#{i(type.primary_key)}",
